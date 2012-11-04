8823daff50055df575273a8d954853779295925d
Ellen Chisa <ellen.chisa@gmail.com>
2012-11-03 22:36:28 -0700

    Setting up rails!

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wanderlust::Application.config.secret_token = '08af6940adce1269a5f7c0ab12bd8f3ada9740a46b078260f2df8c2e52fe568c49a3fc7cfac06af49a3cf1b2e4ab8573453692a6fc3c351738b146a8f1c6e123'
