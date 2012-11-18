1b8d1483c643a97cc179af469b6d3b9cfdf643fa
Ellen Chisa <ellen.chisa@gmail.com>
2012-11-18 09:49:49 -0800

    Add approve control for admin.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,11 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   def isadmin
+    @isadmin = false
     if session[:fbuser] == nil or session[:fbuser]['id'] != '1266930050' then
       redirect_to root_url
+    else
+      @isadmin = true
     end
   end
 
--- app/models/document.rb
+++ app/models/document.rb
@@ -1,6 +1,6 @@
 class Document < ActiveRecord::Base
   
-  attr_accessible :url, :city, :job
+  attr_accessible :url, :city, :job, :approved
   belongs_to :author
   validates_presence_of :url, :city, :job, :author
   
