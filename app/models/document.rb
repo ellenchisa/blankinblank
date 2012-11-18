3775a74de5018822ce2b5bbcae8850b5025b6dd4
Ellen Chisa <ellen.chisa@gmail.com>
2012-11-18 10:27:35 -0800

    Adding Facebook photos for authors.

--- app/models/document.rb
+++ app/models/document.rb
@@ -1,7 +1,8 @@
 class Document < ActiveRecord::Base
   
   attr_accessible :url, :city, :job, :approved
-  belongs_to :author
+  belongs_to :author 
+  default_scope :include => :author
   validates_presence_of :url, :city, :job, :author
   
 end
